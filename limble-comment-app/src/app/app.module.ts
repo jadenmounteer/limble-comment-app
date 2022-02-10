b9441b996188d6e63215bc6054d619d8808ff0b8
jmounteer <mounteerjaden@gmail.com>
2022-02-10 08:15:06 -0700

    Add components, html, and styles

--- limble-comment-app/src/app/app.module.ts
+++ limble-comment-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentsSectionModule } from './comments-section/comments-section.module';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommentsSectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ limble-comment-app/src/app/comments-section/add-new-comment/add-new-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-comment',
+  templateUrl: './add-new-comment.component.html',
+  styleUrls: ['./add-new-comment.component.scss']
+})
+export class AddNewCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
